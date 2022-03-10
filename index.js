09e639570ee6ecec9b9403330f50fd34ea54ce8e
Ozan Gürer <ozangurer12@gmail.com>
2022-03-10 18:39:35 +0300

    Chat

---
+++ index.js
@@ -0,0 +1,48 @@
+const io = require('socket.io')(8900, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId === userId) &&
+        users.push({ userId, socketId })
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId)
+}
+
+io.on("connection", socket => {
+    // Connect;
+    console.log("a user connected")
+
+    //Get UserID and socketID from user
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id)
+        io.emit("getUsers", users)
+    })
+
+    //Post - Get Messages
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        console.log(user)
+        io.to(user.socketId).emit('getMessage', {
+            senderId,
+            text
+        })
+    })
+
+    // Disconnect;
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+        removeUser(socket.id)
+        io.emit("getUsers", users)
+    })
+})
